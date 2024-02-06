c224cc35f807989228a10ce3a25861e4616d6851
Gregory Nelson Esguerra <gregory_keyboard@outlook.com>
2024-02-06 15:38:41 +0800

    Add project files.

---
+++ ConstantInterpolatedStrings/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ConstantInterpolatedStrings
+{
+    internal class Program
+    {
+        private const string FirstName = "John";
+        private const string MiddleName = "Hope";
+        private const string LastName = "Doe";
+        private const string FullName = $"Full Name: {FirstName} {MiddleName} {LastName}";
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(FullName);
+            Console.ReadLine();
+        }
+    }
+}
